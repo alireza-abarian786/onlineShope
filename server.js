bac0576195865c5129f807650167758899862310
alireza-abarian786 <frontdeveloper77@gmail.com>
2025-03-28 02:59:04 +0330

    test fix server 88

--- server.js
+++ server.js
@@ -181,7 +181,7 @@ app.post('/api/carts/:userId/items', async (req, res) => {
     let cart = await Cart.findOne({ user_id: userId });
     if (!cart) {
       cart = new Cart({
-        id: cart.id + '-cart',
+        id: user.id + '-cart',
         user_id: userId,
         items: [],
         totalPrice: 0, // مقدار اولیه
