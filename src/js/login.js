40cff7e464c52acd57aa75c14cfe8c0fab197079
alireza-abarian786 <frontdeveloper77@gmail.com>
2025-04-11 06:55:51 +0330

    fix error signUp user 7

--- src/js/login.js
+++ src/js/login.js
@@ -150,10 +150,10 @@ let statusLogin = async (username) => {
                     throw new Error(errorData.error || `HTTP error! status: ${res.status}`);
                 }
                 
-                loginCheked(username)
-                // setLocalStorage('login' , username);
-                isLogin();
-                clearInputSignUp()
+                // loginCheked(username)
+                // // setLocalStorage('login' , username);
+                // isLogin();
+                // clearInputSignUp()
 
                 hideLoader()
                 Swal.fire({
